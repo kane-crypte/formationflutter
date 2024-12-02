2d61524e90e600dc5234060a797917d9a922f25f
kane-crypte <kane782196004@gmail.com>
2024-12-02 12:30:51 +0000

    premier commite

---
+++ Dart.dart
@@ -0,0 +1,16 @@
+void main() {
+ 
+  String fistname = "ALassane";
+
+  String lastname = "kane";
+
+  String fonction = "Etudiant";
+
+  String Niveau = "Licence 3";
+
+  String specialiter = "Informatique";
+
+  print("Bonjour je me presente \n Prenom : $fistname \n Nom : $lastname \n Fonction : $fonction \n Niveau : $Niveau \n Specialiter :$specialiter");
+}
+
+
